b4072ed24b2e2c76a3357962b9b13330687eabeb
Heisenberg0203 <rushikesh0203@gmail.com>
2020-05-01 21:00:08 +0530

    final commit for tes

---
+++ src/fitness/gets/tes.py
@@ -0,0 +1,161 @@
+import numpy as np
+np.seterr(all="raise")
+import matplotlib.pyplot as plt
+import matplotlib
+import tkinter
+matplotlib.use('TkAgg')
+
+
+from algorithm.parameters import params
+from utilities.fitness.get_data import get_data
+from utilities.fitness.math_functions import *
+from utilities.fitness.optimize_constants import optimize_constants
+
+from fitness.base_ff_classes.base_ff import base_ff
+
+a = np.array([])
+
+class tes(base_ff):
+    """
+    Fitness function for supervised learning, ie regression and
+    classification problems. Given a set of training or test data,
+    returns the error between y (true labels) and yhat (estimated
+    labels).
+
+    We can pass in the error metric and the dataset via the params
+    dictionary. Of error metrics, eg RMSE is suitable for regression,
+    while F1-score, hinge-loss and others are suitable for
+    classification.
+
+    This is an abstract class which exists just to be subclassed:
+    should not be instantiated.
+    """
+
+    def __init__(self):
+        # Initialise base fitness function class.
+        super().__init__()
+
+        # Get training and test data
+        self.training_in, self.training_exp, self.test_in, self.test_exp = \
+            get_data(params['DATASET_TRAIN'], params['DATASET_TEST'])
+
+        # Find number of variables.
+        self.n_vars = np.shape(self.training_in)[0]
+
+        # Regression/classification-style problems use training and test data.
+        if params['DATASET_TEST']:
+            self.training_test = True
+        a = np.array([])
+
+    def evaluate(self, ind, **kwargs):
+        """
+        Note that math functions used in the solutions are imported from either
+        utilities.fitness.math_functions or called from numpy.
+
+        :param ind: An individual to be evaluated.
+        :param kwargs: An optional parameter for problems with training/test
+        data. Specifies the distribution (i.e. training or test) upon which
+        evaluation is to be performed.
+        :return: The fitness of the evaluated individual.
+        """
+        global a
+        dist = kwargs.get('dist', 'training')
+
+        if dist == "training":
+            # Set training datasets.
+            #x = self.training_in
+            x = np.array(self.training_exp)
+        elif dist == "test":
+            # Set test datasets.
+            x = np.array(self.training_exp)
+            test= np.array(self.test_exp)
+            #y = self.test_exp
+
+        else:
+            raise ValueError("Unknown dist: " + dist)
+
+        if params['OPTIMIZE_CONSTANTS']:
+            # if we are training, then optimize the constants by
+            # gradient descent and save the resulting phenotype
+            # string as ind.phenotype_with_c0123 (eg x[0] +
+            # c[0] * x[1]**c[1]) and values for constants as
+            # ind.opt_consts (eg (0.5, 0.7). Later, when testing,
+            # use the saved string and constants to evaluate.
+            if dist == "training":
+                return optimize_constants(x, y, ind)
+
+            else:
+                # this string has been created during training
+                phen = ind.phenotype_consec_consts
+                c = ind.opt_consts
+                # phen will refer to x (ie test_in), and possibly to c
+                yhat = eval(phen)
+                assert np.isrealobj(yhat)
+
+                # let's always call the error function with the
+                # true values first, the estimate second
+                return params['ERROR_METRIC'](y, yhat)
+
+        else:
+            # phenotype won't refer to C
+            d={'list':x}
+            exec(ind.phenotype,d)
+            #print(d.keys())
+            obsereved=x
+            forecast=d['forecast']
+            level=d['level']
+            trend = d['trend']
+            season = d['season']
+            alpha = d['alpha']
+            beta=d['beta']
+            gamma=d['gamma']
+            period=d['period']
+            step = d['step']
+
+            assert np.isrealobj(forecast)
+            if(dist=='test'):
+                valido = test.copy()
+                validlevel=valido.copy()
+                validtrend=valido.copy()
+                validseason=valido.copy()
+                validforecast=valido.copy()
+                
+                for i in range(-step,len(valido)-step):
+                    #print(i,m)
+                    if(i<0):
+                        #print(i+k,len(level)+i,len(level)+i-period+1)        
+                        level[len(level)+i]=level[len(level)-1+i]+trend[len(level)+i-1]+alpha*(obsereved[len(level)+i]-level[len(level)+i-1]-trend[len(level)+i-1]-season[len(season)+i-period])
+                        trend[len(level)+i]=trend[len(level)+i-1]+beta*(level[len(level)+i]-level[len(level)+i-1]-trend[len(level)+i-1])
+                        season[len(level)+i]=season[len(level)+i-period]+gamma*(obsereved[len(level)+i]-level[len(level)+i]-season[len(level)+i-period])
+                        validforecast[i+step]=level[len(level)+i]+step*trend[len(level)+i]+season[len(level)+i-period]
+                    elif(i==0):
+                        #print(i+k,0,len(level)+i-period+1)  
+                        validlevel[0]=level[len(level)-1]+trend[len(level)-1]+alpha*(valido[0]-level[len(level)-1]-trend[len(level)-1]-season[len(season)-period])
+                        validtrend[0]=trend[len(level)-1]+beta*(validlevel[0]-level[len(level)-1]-trend[len(level)-1])
+                        validseason[0]=season[len(level)-period]+gamma*(valido[0]-validlevel[0]-season[len(level)-period])
+                        validforecast[i+step]=validlevel[0]+step*validtrend[0]+season[len(level)+i-period]
+                    else:
+                        
+                        if(i<=(period-1)):
+                            #print(i+k,i,len(level)+i-period+1)
+                            validlevel[i]=validlevel[i-1]+validtrend[i-1]+alpha*(valido[i]-validlevel[i-1]-validtrend[i-1]-season[len(season)+i-period])
+                            validtrend[i]=validtrend[i-1]+beta*(validlevel[i]-validlevel[i-1]-validtrend[i-1])
+                            validseason[i]=season[len(season)+i-period]+gamma*(valido[i]-validlevel[i]-season[len(season)+i-period])
+                            validforecast[i+step]=validlevel[i]+step*validtrend[i] + season[len(level)+i-period]
+                        else:
+                            #print(i+k,i,i+k-period)
+                            validlevel[i]=validlevel[i-1]+validtrend[i-1]+alpha*(valido[i]-validlevel[i-1]-validtrend[i-1]-validseason[i-period])
+                            validtrend[i]=validtrend[i-1]+beta*(validlevel[i]-validlevel[i-1]-validtrend[i-1])
+                            validseason[i]=validseason[i-period]+gamma*(valido[i]-validlevel[i]-validseason[i-period])
+                            validforecast[i+step]=validlevel[i]+step*validtrend[i] + validseason[i+step-period]
+                # plt.figure(figsize = (20,8))
+                # plt.plot(validforecast)
+                # plt.plot(valido)
+                # plt.show()
+                return (np.sqrt(np.mean(np.square(validforecast - valido))))
+                
+
+            # let's always call the error function with the true
+            # values first, the estimate second
+            #a= np.append(a,np.sqrt(np.mean(np.square(x[m:] - forecast[m:]))))
+            return np.sqrt(np.mean(np.square(x[period:] - forecast[period:])))
